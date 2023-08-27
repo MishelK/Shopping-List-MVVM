42dc350b9f75636c9511105711ec8dce5b825841
mishelk <65822657+MishelK@users.noreply.github.com>
2023-08-27 17:32:16 +0300

    Showing data in main activity, connected livedata from shopping items viewmodels to activity

--- app/src/main/java/com/example/shoppinglist/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/shoppinglist/activities/ShoppingActivity.kt
@@ -3,21 +3,44 @@ package com.example.shoppinglist.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
-import com.example.shoppinglist.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.shoppinglist.adapters.ShoppingItemAdapter
+import com.example.shoppinglist.data.items.ShoppingItem
 import com.example.shoppinglist.data.repositories.ShoppingRepository
 import com.example.shoppinglist.data.repositories.db.ShoppingDatabase
+import com.example.shoppinglist.databinding.ActivityShoppingBinding
+import com.example.shoppinglist.dialogs.AddShoppingItemDialog
 import com.example.shoppinglist.viewmodels.ShoppingViewModel
 import com.example.shoppinglist.viewmodels.ShoppingViewModelFactory
 
 class ShoppingActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityShoppingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
+        binding = ActivityShoppingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val repository = ShoppingRepository(ShoppingDatabase(this))
         val factory = ShoppingViewModelFactory(repository)
         val viewModel = ViewModelProvider(this, factory)[ShoppingViewModel::class.java]
+
+        val shoppingItemAdapter = ShoppingItemAdapter(listOf(), viewModel)
+        binding.rvShoppingItems.layoutManager = LinearLayoutManager(this)
+        binding.rvShoppingItems.adapter = shoppingItemAdapter
+
+        viewModel.getAllShoppingItemsLiveData().observe(this) {
+            shoppingItemAdapter.items = it
+            shoppingItemAdapter.notifyDataSetChanged()
+        }
+
+        binding.fab.setOnClickListener {
+            AddShoppingItemDialog(this, object: AddShoppingItemDialog.Listener {
+                override fun onAddShoppingItem(shoppingItem: ShoppingItem) {
+                    viewModel.upsert(shoppingItem)
+                }
+            }).show()
+        }
     }
 }
--- app/src/main/java/com/example/shoppinglist/viewmodels/ShoppingViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/viewmodels/ShoppingViewModel.kt
@@ -11,13 +11,24 @@ import kotlinx.coroutines.launch
 
 class ShoppingViewModel(private val repository: ShoppingRepository): ViewModel() {
 
+    private var shoppingItemsLiveData: MutableLiveData<List<ShoppingItem>> = MutableLiveData()
+
     fun upsert(shoppingItem: ShoppingItem) = CoroutineScope(Dispatchers.IO).launch {
         repository.upsert(shoppingItem)
+        getAllShoppingItemsLiveData()
     }
 
     fun delete(shoppingItem: ShoppingItem) = CoroutineScope(Dispatchers.IO).launch {
         repository.delete(shoppingItem)
+        getAllShoppingItemsLiveData()
     }
 
-    fun getAllShoppingItemsLiveData(): LiveData<List<ShoppingItem>> = MutableLiveData(repository.getAllShoppingItems())
+    fun getAllShoppingItemsLiveData(): LiveData<List<ShoppingItem>> = shoppingItemsLiveData.also {
+        CoroutineScope(Dispatchers.IO).launch {
+            val items = repository.getAllShoppingItems()
+            CoroutineScope(Dispatchers.Main).launch {
+                shoppingItemsLiveData.value = items
+            }
+        }
+    }
 }
